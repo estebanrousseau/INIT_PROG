6c01f0ac388bc3d9979fbe472399b84cf0622dbf
estebanrousseau <esteban.rousseau@etu.univ-orleans.fr>
2024-09-19 10:03:59 +0200

    modif prog

--- TP/tp3/tp3_ex_6.py
+++ TP/tp3/tp3_ex_6.py
@@ -93,6 +93,29 @@ def sup_10(lst):
         return cmp         
 
 
+def somme_neg(lst):
+    """calcule la valeur des nombres négatifs
+
+    Args:
+        lst (list): liste d'entier
+
+    Raises:
+        ValueError: c'est une liste vide 
+
+    Returns:
+        _int : somme des nombres négatifs
+    """
+    
+    if len(lst) == 0 :
+        raise ValueError("la liste est vide")
+    else:
+        cmp = 0
+        for elt in lst:
+            if elt < 0:
+                cmp += elt 
+        return cmp        
+
+
 
 # dernière exercice 
 
@@ -126,4 +149,44 @@ def test_syllabe():
     assert(syllabe('tatatatata')) == 5
     assert(syllabe('a')) == 1 
 
+def test_somme_lst():
+    assert(somme_lst([1, 2, 3, 4, 5])) == 15
+    assert(somme_lst([0, 0, 0, 0, 0])) == 0
+    assert(somme_lst([15, 42, 13, 24, 50])) == 144
+    assert(somme_lst([1 ])) == 1
     
+def test_max_lst():
+    assert(max([1, 2, 3, 4, 5])) == 5
+    assert(max([0, 0, 0, 0, 0])) == 0
+    assert(max([15, 42, 13, 24, 50])) == 50
+    assert(max([1])) == 1
+
+def test_nb_occ():
+    assert(nb_occ("tatata", 'a')) == 3   
+    assert(nb_occ("tatata", 't')) == 3   
+    assert(nb_occ("tttttttt", 'a')) == 0
+    assert(nb_occ("aaaaaaa", 'a')) == 7
+
+def test_min():
+    assert(min([1, 2, 3, 4, 5])) ==  1  
+    assert(min([0, 0, 0, 0, 0])) == 0
+    assert(min([15, 42, 13, 24, 50])) == 13
+    assert(min([1])) ==  1
+
+def test_etendue():
+    assert(etendue([1, 2, 3, 4, 5])) ==  4
+    assert(etendue([0, 0, 0, 0, 0])) ==   0 
+    assert(etendue([15, 42, 13, 24, 50])) == 37   
+    assert(etendue([1])) ==      0
+
+def test_supp_10():
+    assert(sup_10([1, 2, 3, 4, 5])) ==  0    
+    assert(sup_10([11, 11, 11 ])) ==  3   
+    assert(sup_10([15, 42, 13, 24, 50])) == 5    
+    assert(sup_10([10])) ==     0
+
+def test_somme_neg():
+    assert(somme_neg([0,0,0,0,0,0])) == 0
+    assert(somme_neg([-2, -5, -4])) == -11
+    assert(somme_neg([-5, 5, -9, 5])) == -14
+    assert(somme_neg([15, 4, 2])) == 0
