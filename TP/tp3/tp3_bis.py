54788653e1d18847da6196c822a10507ec88b9e0
estebanrousseau <esteban.rousseau@etu.univ-orleans.fr>
2024-09-19 11:47:16 +0200

    yhes

--- TP/tp3/tp3_bis.py
+++ TP/tp3/tp3_bis.py
@@ -12,6 +12,17 @@ def moy (lst):
 
 
 def ecart_typt(lst):
+    """_summary_
+
+    Args:
+        lst (_type_): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
     if lst == []:
         raise ValueError("la liste est vide")
     else:
@@ -19,10 +30,10 @@ def ecart_typt(lst):
         res = 0
         taille_lst = len(lst)
         for n in range(1, taille_lst):
-            res += (1 / n) * (lst[n] - moyenne) ** 2
+            res += (1 / taille_lst) * (lst[n] - moyenne) ** 2
         return sqrt(res)
 
-print(ecart_typt([2, 4, 4, 4, 5, 5, 7, 9]))        
+#print(ecart_typt([2, 4, 4, 4, 5, 5, 7, 9]))        
 
 
 def annee_b(annee):
@@ -50,12 +61,24 @@ def test_annee_b():
     assert(annee_b(2004))
     assert(annee_b(1500))
 
-def divisible_4(annee):
-    return annee %4 == 0
 
 
 def trouve_annee_b(annee1, annee2):
-    dep = 0
-    while divisible_4(annee1 + dep -1 ):
-        dep = 1 
-        annee_b1 += 1 
+    dep = annee1 + 1
+    cmp = 0
+    if annee2 - annee1 > 3 :
+        while  not annee_b(dep) and dep < annee2 :
+            dep += 1    
+
+        for ann in range(dep , annee2   , 4 ):
+            if annee_b(ann):
+                cmp += 1 
+
+    else :
+        for ann in range(annee1 + 1 , annee2):
+            if annee_b(ann):
+                cmp += 1
+
+    return cmp            
+           
+print(trouve_annee_b(1999, 2000))
