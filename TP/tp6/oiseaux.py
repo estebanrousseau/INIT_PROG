ba489b0553f586ca104bd6d77b14219aead085bb
estebanrousseau <esteban.rousseau@etu.univ-orleans.fr>
2024-10-10 10:59:10 +0200

    tp6

---
+++ TP/tp6/oiseaux.py
@@ -0,0 +1,108 @@
+# --------------------------------------
+# DONNEES
+# --------------------------------------
+
+# exemple de liste d'oiseaux observables
+oiseaux = [("Merle", "Turtidé"), ("Moineau", "Passereau"), ("Mésange", "Passereau"),
+           ("Pic vert", "Picidae"), ("Pie", "Corvidé"), ("Pinson", "Passereau"),
+           ("Rouge-gorge", "Passereau"), ("Tourterelle", "Colombidé")] 
+
+# exemples de listes de comptage ces listes ont la même longueur que oiseaux
+comptage1 = [2, 5, 0, 1, 2, 0, 3, 5]
+comptage2 = [2, 1, 3, 0, 0, 3, 5, 1]
+comptage3 = [0, 0, 4, 3, 2, 1, 2, 4]
+
+# exemples de listes d'observations. Notez que chaque liste correspond à la liste de comptage de
+# même numéro
+observations1 = [("Merle", 2), ("Moineau", 5), ("Pic vert", 1), ("Pie", 2),
+                 ("Rouge-gorge", 3), ("Tourterelle", 5)]
+
+observations2 = [("Merle", 2), ("Mésange", 1), ("Moineau", 3),
+                 ("Pinson", 3), ("Tourterelle", 5), ("Rouge-gorge", 1)]
+
+observations3 = [("Mésange", 4), ("Pic vert", 3), ("Pie", 2), ("Pinson", 1),
+                 ("Rouge-gorge", 2), ("Tourterelle", 4)]
+
+
+# --------------------------------------
+# FONCTIONS
+# --------------------------------------
+
+def oiseau_le_plus_observe(liste_observations):
+    """ recherche le nom de l'oiseau le plus observé de la liste
+        (si il y en a plusieur on donne le 1er trouve)
+
+    Args:
+        liste_observations (list): une liste de tuples (nom_oiseau, nb_observes)
+
+    Returns:
+        str: l'oiseau le plus observé (None si la liste est vide)
+    """
+    if liste_observations == []:
+        res = None
+    else:    
+        res = liste_observations[0]
+        for observation in liste_observations:
+            if observation[1] > res[1]:
+                res  = observation
+        res = res[0]        
+    return res
+
+
+
+#--------------------------------------
+# PROGRAMME PRINCIPAL
+#--------------------------------------
+
+# afficher_graphique_observation(construire_liste_observations(oiseaux, comptage3))
+# observes = saisie_observations(oiseaux)
+# afficher_graphique_observation(observes)
+# afficher_observations(oiseaux, observes)
+
+
+def oiseau_le_plus_observe_i(liste_observations):
+    """ recherche le nom de l'oiseau le plus observé de la liste
+        (si il y en a plusieur on donne le 1er trouve)
+
+    Args:
+        liste_observations (list): une liste de tuples (nom_oiseau, nb_observes)
+
+    Returns:
+        str: l'oiseau le plus observé (None si la liste est vide)
+    """
+    if liste_observations == []:
+        res = None
+    else:    
+        res = liste_observations[0]
+        for observation in range(1, len(liste_observations)):
+            if observation[1] > res[1]:
+                res  = observation
+        res = res[0]        
+    return res
+
+
+
+
+#-----------------------------------------------------------------------------------------------
+#exercice 2 
+#-----------------------------------------------------------------------------------------------
+
+
+def caracteristique(lst_oiseau, nom):
+    """permet de retrouver les caractéristiques (nom,famille) d’un oiseau
+       dans une liste d’oiseaux à partir de son nom
+
+    Args:
+        lst_oiseau (list): 
+        nom (str): nom d'oiseau
+
+    Returns:
+        _type_: _description_
+
+    invariant :
+        l'oiseau n'a pas été trouvé    
+    """
+    for woiso in lst_oiseau :
+        if woiso[0] == nom :
+            return woiso
+    return None 
---
+++ TP/tp6/test_oiseaux.py
@@ -0,0 +1,60 @@
+import oiseaux
+# --------------------------------------
+# FONCTIONS
+# --------------------------------------
+
+#def test_recherche_oiseau():
+ #   assert oiseaux.recherche_oiseau(...)==...
+
+#def test_recherche_par_famille():
+  #  assert oiseaux.recherche_par_famille(...)==...
+
+def test_oiseau_le_plus_observe():
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations1)=="Moineau"
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations2)=="Tourterelle"
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations3)=="Mésange"
+    assert oiseaux.oiseau_le_plus_observe([])==None
+
+def test_oiseau_le_plus_observe_i():
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations1)=="Moineau"
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations2)=="Tourterelle"
+    assert oiseaux.oiseau_le_plus_observe(oiseaux.observations3)=="Mésange"
+    assert oiseaux.oiseau_le_plus_observe([])==None    
+
+
+def test_caracteristique():
+    assert oiseaux.caracteristique(oiseaux.oiseaux, 'Merle') == ("Merle", "Turtidé")
+    assert oiseaux.caracteristique(oiseaux.oiseaux, 'Tourterelle') == ("Tourterelle", "Colombidé")
+    assert oiseaux.caracteristique(oiseaux.oiseaux, 'Pie') == ("Pie", "Corvidé")
+    assert oiseaux.caracteristique(oiseaux.oiseaux, 'Pic vert') == ("Pic vert", "Picidae")
+
+
+
+
+
+
+"""
+def test_est_liste_observations():
+    assert oiseaux.est_liste_observations(...)==...
+
+def test_max_observations():
+    assert oiseaux.max_observations(...)==...
+
+def test_moyenne_oiseaux_observes():
+    assert oiseaux.moyenne_oiseaux_observes(...)==...
+
+def test_total_famille():
+    assert oiseaux.total_famille(...)==...
+
+
+def test_construire_liste_observations():
+    assert oiseaux.construire_liste_observations(...)==...
+
+def test_creer_ligne_sup():
+    assert oiseaux.creer_ligne_sup(...)==...
+
+def test_creer_ligne_noms_oiseaux():
+    assert oiseaux.creer_ligne_noms_oiseaux(...)==...
+
+
+"""
